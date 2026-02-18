81704b9325715f2cebd22caae2b6f2c82c0d6445
Friedrich Ewald <fewald@linkedin.com>
2026-02-18 09:14:49 -0800

    Initial Commit

---
+++ ultraphotos/ultraphotosApp.swift
@@ -0,0 +1,32 @@
+//
+//  ultraphotosApp.swift
+//  ultraphotos
+//
+//  Created by Friedrich Ewald on 2/18/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ultraphotosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ultraphotosTests/ultraphotosTests.swift
@@ -0,0 +1,17 @@
+//
+//  ultraphotosTests.swift
+//  ultraphotosTests
+//
+//  Created by Friedrich Ewald on 2/18/26.
+//
+
+import Testing
+@testable import ultraphotos
+
+struct ultraphotosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
